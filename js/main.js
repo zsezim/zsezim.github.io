374d1857cd96cc8670ce0c1713fa56eb161a25b0
Sezim Zamirbekova <zsezim@gmail.com>
2025-07-28 14:43:36 -0500

    Fix indentations

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // Parameters
 let currentScene = 0;
 let selectedX = "gdp"; // exploration scene parameter
@@ -6,6 +5,7 @@ let covidData;
 
 const scenes = [sceneIntro, sceneGDP, sceneHDI, sceneAge, sceneExplore];
 
+// Load data and render the first scene
 d3.csv("data/owid-covid-data.csv").then(data => {
   covidData = data.map(d => ({
     country: d.location,
@@ -17,11 +17,13 @@ d3.csv("data/owid-covid-data.csv").then(data => {
   renderScene(currentScene);
 });
 
+// Scene renderer
 function renderScene(index) {
   d3.select("#viz").html("");
   scenes[index]();
 }
 
+// Scene 1: Intro bar chart
 function sceneIntro() {
   const svg = d3.select("#viz").append("svg")
     .attr("width", 800)
@@ -54,11 +56,14 @@ function sceneIntro() {
     .attr("fill", "crimson");
 
   g.append("g").call(d3.axisLeft(y));
-  g.append("g").attr("transform", `translate(0, 400)`).call(d3.axisBottom(x));
+  g.append("g")
+    .attr("transform", `translate(0, 400)`)
+    .call(d3.axisBottom(x));
 }
 
+// Scatterplot generator with annotations
 function drawScatter(xKey, xLabel) {
-  const margin = {top: 50, right: 50, bottom: 60, left: 80};
+  const margin = { top: 50, right: 50, bottom: 60, left: 80 };
   const width = 800 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
 
@@ -78,11 +83,14 @@ function drawScatter(xKey, xLabel) {
     .domain(d3.extent(filteredData, d => d.deaths)).nice()
     .range([height, 0]);
 
+  // Axes
   svg.append("g")
     .attr("transform", `translate(0,${height})`)
     .call(d3.axisBottom(x));
-  svg.append("g").call(d3.axisLeft(y));
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
+  // Axis labels
   svg.append("text")
     .attr("x", width / 2)
     .attr("y", height + 40)
@@ -96,6 +104,7 @@ function drawScatter(xKey, xLabel) {
     .attr("text-anchor", "middle")
     .text("COVID-19 Deaths per Million");
 
+  // Scatter dots
   svg.selectAll("circle")
     .data(filteredData)
     .enter()
@@ -107,9 +116,7 @@ function drawScatter(xKey, xLabel) {
     .append("title")
     .text(d => `${d.country}: ${d.deaths.toFixed(1)} deaths`);
 
-  // Safe Annotations
-  // Safe Annotations
-
+  // Hardcoded annotation points
   const maxCountry = {
     country: "Peru",
     deaths: 6601.11,
@@ -117,7 +124,7 @@ function drawScatter(xKey, xLabel) {
     hdi: 0.77,
     age: 29.1
   };
-  
+
   const minCountry = {
     country: "Burundi",
     deaths: 1.13,
@@ -125,7 +132,7 @@ function drawScatter(xKey, xLabel) {
     hdi: 0.43,
     age: 17.5
   };
-  
+
   const annotations = [
     {
       note: {
@@ -152,29 +159,33 @@ function drawScatter(xKey, xLabel) {
   ];
 
   const makeAnnotations = d3.annotation()
-  .type(d3.annotationLabel)
-  .annotations(annotations);
+    .type(d3.annotationLabel)
+    .annotations(annotations);
 
-svg.append("g")
-  .attr("class", "annotation-group")
-  .call(makeAnnotations);
+  svg.append("g")
+    .attr("class", "annotation-group")
+    .call(makeAnnotations);
 }
 
+// Scene 2: GDP scatterplot
 function sceneGDP() {
   selectedX = "gdp";
   drawScatter(selectedX, "GDP per Capita ($)");
 }
 
+// Scene 3: HDI scatterplot
 function sceneHDI() {
   selectedX = "hdi";
   drawScatter(selectedX, "Human Development Index");
 }
 
+// Scene 4: Age scatterplot
 function sceneAge() {
   selectedX = "age";
   drawScatter(selectedX, "Median Age");
 }
 
+// Scene 5: Interactive exploration
 function sceneExplore() {
   const container = d3.select("#viz");
 
@@ -210,6 +221,7 @@ function sceneExplore() {
   });
 }
 
+// Navigation buttons
 d3.select("#next").on("click", () => {
   if (currentScene < scenes.length - 1) {
     currentScene++;
